fa067cbeb7d2fc2860b6f358d45934511a5eb796
PALAGIRI SULTHAN <sulthanpalagirisulthan@gmail.com>
2025-08-30 05:54:46 +0000

    c

---
+++ snapshots/snap_data_snp_raw.sql
@@ -0,0 +1,14 @@
+{% snapshot  snap_data_snp_raw %}
+
+{{
+    config(
+      target_schema='snapshots',
+      strategy='timestamp',
+      unique_key='id',
+      updated_at='updated_at'
+   )
+}}
+select * 
+from {{source('datafeed_shared_schema','data_snp_raw')}}
+
+{% endsnapshot %}
